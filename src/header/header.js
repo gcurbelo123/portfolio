e5d6b2f1f9c9ae34b435fb33c90d751a4d13b3a7
gcurbelo123 <gilbertcurbelo3@gmail.com>
2020-05-31 15:22:39 -0700

    Added Animation to Cover

--- src/header/header.js
+++ src/header/header.js
@@ -1,89 +1,104 @@
 import React from 'react';
-import { styled, withStyle } from 'styletron-react';
+import styled, { keyframes } from 'styled-components';
+// import { styled, withStyle } from 'styletron-react';
 import { useHistory } from 'react-router-dom';
 import ComicPhotoCover from '../images/gc.png';
 import FirstApp from '../images/sticker.png';
 import PriceQR from '../images/comicPurchase.png';
 
-const CoverPage = styled('div', {
-  width: '100%',
-  minHeight: '100%',
-  position: 'fixed',
-  backgroundColor: 'black',
-  overflow: 'scroll',
-});
+const CoverPage = styled.div`
+  width: 100%;
+  min-height: 100%;
+  position: fixed;
+  background-color: black;
+  overflow: scroll;
+`;
 
-const ComicBook = styled('div', {
-  width: '90%',
-  marginLeft: '5%',
-  position: 'absolute',
-  backgroundColor: '#003366',
-  display: 'flex',
-  justifyContent: 'center',
-  border: '1px solid white',
-});
+const ComicBook = styled.div`
+  width: 90%;
+  margin-left: 5%;
+  position: absolute;
+  background-color: #003366;
+  display: flex;
+  justify-content: center;
+  border: 1px solid white;
+`;
 
-const ImgBackground = styled('img', {
-  zIndex: 0,
-  top: 0,
-  width: '80%',
-  marginTop: '15%',
-  justifyContent: 'center',
-});
+const ImgBackground = styled.img`
+  z-index: 0;
+  top: 0;
+  width: 80%;
+  margin-top: 15%;
+  justify-content: center;
+`;
 
-const HeaderTextBox = styled('div', {
-  top: 0,
-  position: 'absolute',
-  letterSpacing: '4px',
-  marginTop: '5%',
-  textAlign: 'center',
-});
+const HeaderTextBox = styled.div`
+  top: 0;
+  position: absolute;
+  letter-spacing: 4px;
+  margin-top: 5%;
+  text-align: center;
+`;
 
-const HeaderText = styled('div', {
-  fontSize: '64px',
-  color: 'white',
-  fontFamily: 'Bangers',
-  position: 'relative',
-  display: 'flex',
-  justifyContent: 'center',
-});
+const HeaderText = styled.div`
+  font-size: 64px;
+  color: white;
+  font-family: Bangers;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
 
-const NameText = withStyle(HeaderText, {
-  fontSize: '136px',
-  textShadow: '0px 0px 32px #000000',
-});
+const nameEnter = keyframes`
+  0% {
+    font-size: 0px;
+  }
+  100% {
+    font-size: 136px;
+  }
+`;
 
-const DecalBox = styled('div', {
-  top: 0,
-  right: 0,
-  position: 'absolute',
-  marginTop: '25%',
-  marginRight: '3%',
-  display: 'flex',
-});
+const NameText = styled.div`
+  color: white;
+  font-family: Bangers;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  text-shadow: 0px 0px 32px #000000;
+  animation: 1.5s ${nameEnter} forwards;
+`;
 
-const DecalSticker = styled('img', {
-  zIndex: 3,
-  position: 'absolute',
-  right: 0,
-  top: 0,
-  height: 'fit-content',
-  width: 'fit-content',
-  ':hover': {
-    cursor: 'pointer',
-  },
-});
+const DecalBox = styled.div`
+  top: 0;
+  right: 0;
+  position: absolute;
+  margin-top: 25%;
+  margin-right: 3%;
+  display: flex;
+`;
 
-const QRPlace = styled('img', {
-  position: 'absolute',
-  marginTop: '100%',
-  marginLeft: '5%',
-  left: 0,
-  top: 0,
-  ':hover': {
-    cursor: 'pointer',
-  },
-});
+const DecalSticker = styled.img`
+  z-index: 3;
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: fit-content;
+  width: fit-content;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const QRPlace = styled.img`
+  position: absolute;
+  margin-top: 100%;
+  margin-left: 5%;
+  left: 0;
+  top: 0;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const ComicBookCover = () => {
   const history = useHistory();
